7f24c598838c8dd3ef16ca09abeca9a5de3a4358
unknown <gulam.mostafa209@gmail.com>
2022-12-17 21:40:34 +0600

    btn

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const home = () => {
     return (
         <div>
             <h2 className='text-center my-12'>Home page</h2>
+            <Link className='btn ' href='/'>GO home</Link>
         </div>
     );
 };
